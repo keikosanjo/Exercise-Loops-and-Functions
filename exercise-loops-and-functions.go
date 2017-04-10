acbb1f56fbe85a3241ed8b9e88bd57d0073b46c8
unknown <>
2017-04-11 01:46:39 +0900

    loop stop and number of loops

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,23 +5,29 @@ import (
 	"math"
 )
 
-func Sqrt(x float64) float64 {
+func Sqrt(x float64) (float64, int) {
 	z := float64(1)
-	var Z float64
-	for {
+	var I int
+	for i := 0; i < 1000; i++ {
+		var Z float64
+
 		Z = z
 		z = z - (math.Pow(z, 2)-x)/(2*z)
 
-		if math.Abs(Z-z) < 1e-10 {
-			return z
+		if math.Abs(Z-z) <= 1e-10 {
+			I = i
+			break
 		}
 
 	}
+	return z, I
 
 }
 
 func main() {
-	fmt.Println(Sqrt(2))
+	z, i := Sqrt(2)
+	fmt.Println(i)
+	fmt.Println(z)
 	fmt.Println(math.Sqrt(2))
 }
 
