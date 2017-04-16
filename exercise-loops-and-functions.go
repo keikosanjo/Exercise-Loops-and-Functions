038cdda6c84fe51ecd5769bc7a96ff9543c5fa0d
unknown <>
2017-04-16 23:04:03 +0900

    fix

--- exercise-loops-and-functions.go
+++ exercise-loops-and-functions.go
@@ -5,28 +5,24 @@ import (
 	"math"
 )
 
-func Sqrt(x float64) (float64, int) {
+func Sqrt(x float64) (float64) {
 	z := float64(1)
-	var I int
 	for i := 0; i < 1000; i++ {
-		var Z float64
 
-		Z = z
+		Z := z
 		z = z - (math.Pow(z, 2)-x)/(2*z)
 
 		if math.Abs(Z-z) <= 1e-10 {
-			I = i
 			break
 		}
 
 	}
-	return z, I
+	return z
 
 }
 
 func main() {
-	z, i := Sqrt(2)
-	fmt.Println(i)
+	z := Sqrt(2)
 	fmt.Println(z)
 	fmt.Println(math.Sqrt(2))
 }
