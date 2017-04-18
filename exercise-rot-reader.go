7abdfee026ad055c42640214d361bfa28b75082e
unknown <>
2017-04-18 22:44:02 +0900

    make func rot13Reader

---
+++ exercise-rot-reader.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"io"
+	"os"
+	"strings"
+)
+
+type rot13Reader struct {
+	r io.Reader
+}
+
+func (r *rot13Reader) Read(b []byte) (int, error) {
+	n, err := r.r.Read(b)
+
+	
+	return n, err
+}
+
+func main() {
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
+}
