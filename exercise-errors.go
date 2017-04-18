a9fa1a777be91db4731b721540d5ea9fba204918
unknown <>
2017-04-18 22:38:19 +0900

    make func Sqrt

--- exercise-errors.go
+++ exercise-errors.go
@@ -12,6 +12,26 @@ func (e ErrNegativeSqrt) Error() string {
 
 }
 
+func Sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, ErrNegativeSqrt(x)
+	}
+
+	z := float64(1)
+	Z := z
+	for i := 0; i < 10; i++ {
+
+		z = z - (math.Pow(z, 2)-x)/(2*z)
+
+		if math.Abs(Z-z) <= 1e-10 {
+			break
+		}
+		Z = z
+	}
+
+	return z, nil
+}
+
 func main() {
 	fmt.Println(Sqrt(-2))
 	fmt.Println(Sqrt(2))
