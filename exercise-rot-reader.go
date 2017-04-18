f6f8d96a0928f415a625707462554c18cae8bd0c
unknown <>
2017-04-18 22:49:00 +0900

    do ROT13

--- exercise-rot-reader.go
+++ exercise-rot-reader.go
@@ -13,7 +13,21 @@ type rot13Reader struct {
 func (r *rot13Reader) Read(b []byte) (int, error) {
 	n, err := r.r.Read(b)
 
-	
+	if err != nil {
+	} else {
+		for i := 0; i < n; i++ {
+			if b[i] >= 'a' && b[i] <= 'm' {
+				b[i] = b[i] + 13
+			} else if b[i] > 'm' && b[i] <= 'z' {
+				b[i] = b[i] - 13
+			} else if b[i] >= 'A' && b[i] <= 'M' {
+				b[i] = b[i] + 13
+			} else if b[i] > 'M' && b[i] <= 'Z' {
+				b[i] = b[i] - 13
+
+			}
+		}
+	}
 	return n, err
 }
 
